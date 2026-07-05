83601de1f815e0b77ba2977723e192d38f139dc2
Jussi Kämäräinen <jussi.kamarainen@iki.fi>
2026-07-05 14:44:59 +0300

    fix(automations): do not restart kanata

--- modules/automations.lua
+++ modules/automations.lua
@@ -6,9 +6,6 @@ watcher = hs.caffeinate.watcher.new(
             --eventType == hs.caffeinate.watcher.screensDidWake
         )
         then
-            -- Restart Kanata
-            hs.execute("kanata-service restart", true)
-            print("Wakeup: Restarted Kanata")
             -- Restart BetterMouse
             hs.osascript.applescript([[quit app "BetterMouse"]])
             hs.osascript.applescript([[tell application "BetterMouse" to activate]])
